be17f0957af9c1e8953ea793ffe8748aab1edacd
Sayantan <sayantanhalder43@gmail.com>
2025-08-22 01:39:41 +0530

    Added pass by reference method

--- Day 52/Swapnosusingpointer.cpp
+++ Day 52/Swapnosusingpointer.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 using namespace std;
 
+// Pass By pointers
+
 void swap(int *p1, int *p2) // arguement as pointers which *(derefence operator) *p is pointing to the value of the respective address
 {
     *p1 = *p1 ^ *p2;
     *p2 = *p1 ^ *p2;
     *p1 = *p1 ^ *p2;
+}
+
+// to avoid pointers we can use reference variables
+// Pass By reference
+
+void swap(int &p1, int &p2)
+{
+    p1 = p1 ^ p2;
+    p2 = p1 ^ p2;
+    p1 = p1 ^ p2;
 }
 
 int main()
@@ -17,7 +29,9 @@ int main()
     cout << "Enter the value of n2: ";
     cin >> n2;
 
-    swap(&n1, &n2); // passing the adress of n1 and n2
+    // swap(&n1, &n2); // passing the adress of n1 and n2
+    // For pass by refence we dont need to pass the adress
+    swap(n1, n2);
 
     cout << "The value of n1 and n2 after swap: " << n1 << " and " << n2;
     return 0;
