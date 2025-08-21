8664fd60284ee8b848802a189de7a3ffbc46b697
Sayantan <sayantanhalder43@gmail.com>
2025-08-22 01:16:35 +0530

    modified comment

--- Day 52/Swapnosusingpointer.cpp
+++ Day 52/Swapnosusingpointer.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void swap(int *p1, int *p2)
+void swap(int *p1, int *p2) // arguement as pointers which *(derefence operator) *p is pointing to the value of the respective address
 {
     *p1 = *p1 ^ *p2;
     *p2 = *p1 ^ *p2;
@@ -17,7 +17,7 @@ int main()
     cout << "Enter the value of n2: ";
     cin >> n2;
 
-    swap(&n1, &n2);
+    swap(&n1, &n2); // passing the adress of n1 and n2
 
     cout << "The value of n1 and n2 after swap: " << n1 << " and " << n2;
     return 0;
