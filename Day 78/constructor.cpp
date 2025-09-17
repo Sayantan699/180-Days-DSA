25467159a7c7fc92ecb331d19b244fd820589c06
Sayantan <sayantanhalder43@gmail.com>
2025-09-17 20:08:52 +0530

    Added parameterized constructor

--- Day 78/constructor.cpp
+++ Day 78/constructor.cpp
@@ -17,9 +17,18 @@ public:
         balance = 10;
     }
 
+    // Parameterized Constructor
+    Customer(string name, int acc_number, int balance)
+    {
+        // this stores the address of the object created..
+        this->name = name;
+        this->acc_number = acc_number;
+        this->balance = balance;
+    }
+
     void display()
     {
-        cout << name << " " << acc_number << " " << balance;
+        cout << name << " " << acc_number << " " << balance << endl;
     }
 };
 
@@ -27,4 +36,8 @@ int main()
 {
     Customer A1; // Object is being formed!!
     A1.display();
+
+    Customer A2;
+    ;
+    A2.display();
 }
