7b32d73c5063d93cacf21558a77b96605efb4006
Sayantan <sayantanhalder43@gmail.com>
2025-09-21 16:12:32 +0530

    first commit

---
+++ Day 83/ExceptionHandling.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+class Customer
+{
+
+    string name;
+    int balance;
+    int acc_number;
+
+public:
+    Customer() {}
+
+    Customer(string name, int balance, int acc_number)
+    {
+        this->name = name;
+        this->balance = balance;
+        this->acc_number = acc_number;
+    }
+
+    void deposit(int amount)
+    {
+        if (amount <= 0)
+            throw "Invalid amount!!"; // Remaining statement will not execute
+        balance += amount;
+        cout << amount << "rs credited Successfully!!" << endl;
+    }
+
+    void withdraw(int amount)
+    {
+        if (amount <= 0 || amount > balance)
+            throw "Invalid amount!!"; // Remaining statement will not execute
+        balance -= amount;
+        cout << amount << "rs withdraws Successfully!!" << endl;
+    }
+};
+
+int main()
+{
+    Customer C("Sayantan", 5000, 620);
+
+    try
+    {
+        C.deposit(100);
+        C.withdraw(6000); // If exception occurs no statement will execute after this
+        C.deposit(200);
+    }
+    catch (const char *e)
+    {
+        cout << "Exception Occured: " << e << endl; // e stores the throw statement
+    }
+}
