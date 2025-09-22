5b6c50f96e6f0478c3dd7b39ebd70a1c8b8c0c08
Sayantan <sayantanhalder43@gmail.com>
2025-09-22 23:14:48 +0530

    Modified and added to print LL

--- Day 84/InsertLL.cpp
+++ Day 84/InsertLL.cpp
@@ -16,7 +16,8 @@ public:
 
 int main()
 {
-    Node *Head;
+    // Initialize Head to NULL to correctly represent an empty list
+    Node *Head = NULL;
 
     Node *temp;
     temp = new Node(45); // New Node
@@ -26,9 +27,20 @@ int main()
     { // Empty LL i.e at first create the first node
         Head = new Node(5);
     }
-    else
-    {                      // Insert the new node at the start of LL
-        temp->next = Head; // point the temp to the head node of LL
-        Head = temp;       // Make that temp as Head node of LL
+    // Insert the new node at the start of LL
+    temp->next = Head; // point the temp to the head node of LL
+    Head = temp;       // Make that temp as Head node of LL
+
+    // To print our LL
+
+    // Create a another temporary pointer which point at head and is used to traverse the LL
+
+    Node *temp1 = Head;
+    while (temp1 != NULL)
+    {
+        cout << temp1->data << "->";
+        temp1 = temp1->next;
     }
+    cout << "NULL";
+    return 0;
 }
