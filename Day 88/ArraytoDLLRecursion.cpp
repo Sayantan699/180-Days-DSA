34d9428cad5e19bdea75a8ee1409c580ecc42bda
Sayantan <sayantanhalder43@gmail.com>
2025-09-28 16:56:35 +0530

    first commit

---
+++ Day 88/ArraytoDLLRecursion.cpp
@@ -0,0 +1,65 @@
+// Convert Array into Doubly Linked List
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *prev;
+    Node *next;
+    Node(int value)
+    {
+        data = value;
+        prev = NULL;
+        next = NULL;
+    }
+};
+
+Node *CreateLL(int arr[], int size, int ind, Node *prevNode = NULL)
+{
+    if (ind == size)
+    {
+        return NULL;
+    }
+    Node *temp = new Node(arr[ind]);
+    temp->prev = prevNode;
+    temp->next = CreateLL(arr, size, ind + 1, temp);
+    return temp;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "Elements of the array are: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
+    Node *Head = CreateLL(arr, n, 0);
+
+    Node *ptr = Head;
+
+    while (ptr != NULL)
+    {
+        cout << ptr->data << "->";
+        ptr = ptr->next;
+    }
+
+    cout << "NULL";
+
+    return 0;
+}
