714a739eba4deee017826b2bf1fd6495ae108ef8
Sayantan <sayantanhalder43@gmail.com>
2025-09-30 12:47:45 +0530

    first commit

---
+++ Day 92/ArraytoCLL.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int value)
+    {
+        data = value;
+        next = NULL;
+    }
+};
+
+Node *CreateCLL(int arr[], int size)
+{
+    if (size == 0)
+        return NULL;
+
+    Node *head = new Node(arr[0]);
+    Node *last = head;
+
+    for (int i = 1; i < size; i++)
+    {
+        Node *temp = new Node(arr[i]);
+        last->next = temp; // forward link only
+        last = temp;
+    }
+
+    // close the circle
+    last->next = head;
+
+    return head;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // Create CLL directly
+    Node *Head = CreateCLL(arr, n);
+
+    // Print CLL (print n times to avoid infinite loop)
+    cout << "Circular Linked List: ";
+    Node *ptr = Head;
+    for (int i = 0; i < n; i++)
+    {
+        cout << ptr->data << "->";
+        ptr = ptr->next;
+    }
+    cout << "(back to Head)" << endl;
+
+    return 0;
+}
