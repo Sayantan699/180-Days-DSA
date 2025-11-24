a33415fe109724bfc2721b279cc15415d8c4640c
Sayantan699 <sayantanhalder43@gmail.com>
2025-11-24 19:05:41 +0530

    added

--- Day 147/PrintBSTinRange.cpp
+++ Day 147/PrintBSTinRange.cpp
@@ -1,5 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
 class Solution {
   public:
     void Find(Node* root,vector<int> &ans,int n1,int n2){
